63ae3aba684e1d3eac4285c7d806c96e92ae7061
ProBaconHub <ProBaconHub@gmail.com>
2025-09-03 17:21:27 +0800

    Update FREEMIUM.lua

--- Mad City: Chapter 2/FREEMIUM.lua
+++ Mad City: Chapter 2/FREEMIUM.lua
@@ -4,4 +4,11 @@ return {
     ["MONTH"] = 10,
     ["DAY"] = 30
     ["KEY"] = "",
+    ["BANK"] = true,
+    ["CLUB"] = true,
+    ["JEWELRY"] = true,
+    ["PLANE"] = true,
+    ["PYRAMID"] = true,
+    ["RESORT"] = true,
+    ["CARGO"] = true,
 }
