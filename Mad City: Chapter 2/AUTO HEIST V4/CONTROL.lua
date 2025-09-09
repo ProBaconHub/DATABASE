0974122cc70bf7941bf6978bab44f0fdc3845bbf
ProBaconHub <ProBaconHub@gmail.com>
2025-09-10 00:58:52 +0800

    Update CONTROL.lua

--- Mad City: Chapter 2/AUTO HEIST V4/CONTROL.lua
+++ Mad City: Chapter 2/AUTO HEIST V4/CONTROL.lua
@@ -1,6 +1,6 @@
 return {
     ["ENABLED"] = true,
-    ["VERSION"] = "b4.2.0_2F2BBE3B6A",
+    ["VERSION"] = "b4.3.0_2F2BCD2F8B",
     ["Bank"] = true,
     ["Club"] = true,
     ["Jewelry"] = true,
